710b86ea08ef3ffccca443a7afe04c947086c48f
Juaneth <euanwalker2008@gmail.com>
2021-12-20 18:10:41 +0000

    gorksh

--- website/entity-docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/raspberrypi/Entity/website/entity-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/raspberrypi/Entity/website/entity-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/raspberrypi/Entity/website/entity-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/home/raspberrypi/Entity/website/entity-docs/src/css/custom.css"),
-];
--- website/entity-docs/docs/get-started.md
+++
@@ -1,7 +0,0 @@
----
-sidebar_position: 1
----
-
-# Getting Started
-
-If you want to start learning Entity, follow this to get started with the language and how to get it set up.
